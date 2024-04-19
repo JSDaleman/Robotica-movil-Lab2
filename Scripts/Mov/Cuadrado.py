58d9d6bcac7023553f8c8c6054e4b1a181ca8186
JSDaleman <70998067+JSDaleman@users.noreply.github.com>
2024-04-18 19:18:31 -0500

    Update Cuadrado.py

--- Scripts/Mov/Cuadrado.py
+++ Scripts/Mov/Cuadrado.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from ev3dev2.motor import OUTPUT_B, OUTPUT_C, MoveTank, SpeedPercent
 from ev3dev2.sensor.lego import GyroSensor
 import time
@@ -8,7 +10,7 @@ SPEED = SpeedPercent(30)
 
 # Función para girar 90 grados
 def girar_90_grados(tank):
-    tank.turn_degrees(90, speed=SPEED)
+    tank.turn_degrees(speed=SPEED, target_angle=90 )
 
 # Función para moverse hacia adelante
 def moverse_adelante(tank):
@@ -16,19 +18,22 @@ def moverse_adelante(tank):
 
 # Inicializar objetos del motor y el giroscopio
 tank = MoveTank(OUTPUT_B, OUTPUT_C)
-gyro = GyroSensor()
+tank.gyro = GyroSensor()
 
 # Calibrar el giroscopio
-gyro.calibrate()
-
+tank.gyro.calibrate()
+print("Angulo inicial:", tank.gyro.angle)
 # Esperar a que el giroscopio se calibre completamente
-while gyro.calibrating:
+while tank.gyro.calibrate():
     time.sleep(0.1)
 
 # Recorrer el cuadrado
 for _ in range(4):
     moverse_adelante(tank)  # Moverse hacia adelante (lado del cuadrado)
+    time.sleep(2)
     girar_90_grados(tank)   # Girar 90 grados
-
+    time.sleep(2)
+    angle = tank.gyro.angle
+    print("Angulo actual:", angle)
 # Detener los motores al final
 tank.stop()
